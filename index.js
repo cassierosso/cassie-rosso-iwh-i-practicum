9cb3d6cbd2a713bb603db99db8edfde58d43b8ad
cassierosso <97743727+cassierosso@users.noreply.github.com>
2024-12-19 12:27:49 -0600

    Create Homepage Template and index updates

--- index.js
+++ index.js
@@ -18,12 +18,38 @@ app.get("/", async (req, res) => {});
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-app.get("/update-cobj", async (req, res) => {});
+app.get("/update-cobj", async (req, res) => {
+	res.render("updates", {
+		title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+	});
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-app.post("/update-cobj", async (req, res) => {});
+app.post("/update-cobj", async (req, res) => {
+	const update = {
+		properties: {
+			jersey_number: req.body.jersey_number,
+			position: req.body.position,
+			name: req.body.name,
+		},
+	};
+	const objectId = req.body.objectId;
+	const updatePlayer = `https://api.hubspot.com/crm/v3/objects/2-38362414/${objectId}`;
+	const headers = {
+		Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+		"Content-Type": "application/json",
+	};
+
+	try {
+		await axios.patch(updatePlayer, update, { headers });
+		res.redirect("back");
+	} catch (err) {
+		console.error(err);
+	}
+});
+
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
